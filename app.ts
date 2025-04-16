2e086bd7e6afa4a31db062be9b1b18aed3b0bd09
Darien Bernal <darien.bernal@gmail.com>
2025-04-16 01:32:34 -0400

    Made changes in main file

--- app.ts
+++ app.ts
@@ -1,8 +1,8 @@
-let message: string = 'Hello, World!';
+let message: string = 'Hello, TypeScript!';
 
-// create a new heading 1 element
+// Create a new heading 1 element
 let heading = document.createElement('h1');
 heading.textContent = message;
 
-// add the heading the document
+// Add the heading the document
 document.body.appendChild(heading);
